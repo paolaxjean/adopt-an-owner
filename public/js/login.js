043c19c97c770294f51b28dd06be0862fd48e62d
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-05-11 19:39:38 -0700

    added dog pic error code to login.js and commented out the sign up function that wasnt doing anything

--- public/js/login.js
+++ public/js/login.js
@@ -2,52 +2,52 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert(response.statusText);
+      document.location.replace("https://http.dog/404.jpg");
     }
   }
 };
+// THIS FUNCTION BELOW DOES NOT SEEM TO AFFECT THE SIGN UP FORM
+// const signupFormHandler = async (event) => {
+//   event.preventDefault();
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
+//   const name = document.querySelector("#name").value.trim();
+//   const email = document.querySelector("#email").value.trim();
+//   const password = document.querySelector("#password").value.trim();
 
-  const name = document.querySelector('#name').value.trim();
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
+//   if (name && email && password) {
+//     const response = await fetch("/api/users", {
+//       method: "POST",
+//       body: JSON.stringify({ name, email, password }),
+//       headers: { "Content-Type": "application/json" },
+//     });
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// };
 
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
