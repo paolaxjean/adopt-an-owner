f9a0b49241cd49a1029065496ddf6040fdb642cc
paolaxjean <pr1008631@gmail.com>
2022-05-12 20:27:46 -0700

    cleaned up code

--- public/js/login.js
+++ public/js/login.js
@@ -21,33 +21,7 @@ const loginFormHandler = async (event) => {
     }
   }
 };
-// THIS FUNCTION BELOW DOES NOT SEEM TO AFFECT THE SIGN UP FORM
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const name = document.querySelector("#name").value.trim();
-//   const email = document.querySelector("#email").value.trim();
-//   const password = document.querySelector("#password").value.trim();
-
-//   if (name && email && password) {
-//     const response = await fetch("/api/users", {
-//       method: "POST",
-//       body: JSON.stringify({ name, email, password }),
-//       headers: { "Content-Type": "application/json" },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace("/");
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
 
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
